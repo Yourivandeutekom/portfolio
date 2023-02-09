40613caacb0ce06e947b282de64a21ccee10cf18
Yourivandeutekom <yourivandeutekom@gmail.com>
2023-02-09 13:40:19 +0100

    Added functionality to the portfolio items, clicking on a portfolio items opens a box now

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
 
+    // Navigation
     const navigation = document.getElementById('navigation');
 
     window.onscroll = () => {
@@ -13,4 +14,62 @@ window.addEventListener('load', () => {
             navigation.classList.remove('scroll');
         }
     }
+
+    // Portfolio items
+    const projects = document.getElementById('projects-container');
+    const projectsCloseBtn = document.getElementById('projects-close-btn');
+    const projectsOpacityLayer = document.getElementById('projects-opacity-layer');
+    const projectsItem = document.querySelectorAll('#projects li');
+    let currentProject;
+    
+    const projectsHandler = index => {
+        showProject(index);
+    };
+
+    const closeHandler = () => {
+        hideProject();
+    };
+
+    const showProject = (index) => {
+
+        let projectEntryClass = '.project-entry-' + (index + 1);
+        currentProject = document.querySelector(projectEntryClass);
+
+        visibility([projects, projectsCloseBtn, projectsOpacityLayer]);
+        currentProject.style.display = 'flex';
+    };
+
+    const visibility = (array) => {
+        array.forEach(variable => {
+            variable.style.visibility = 'visible';
+            variable.classList.add('activated');
+        });
+    };
+
+    const hideProject = () => {
+
+        hidden([projectsCloseBtn, projects, projectsOpacityLayer]);
+        currentProject.style.display = 'none';
+
+        currentProject = undefined;
+    };
+
+    const hidden = (array) => {
+        array.forEach(variable => {
+            variable.classList.remove('activated');
+
+            setTimeout(() => {
+                variable.style.visibility = 'hidden';
+            }, 300);
+        });
+    };
+
+    projectsItem.forEach((event, index) => {
+        event.addEventListener('click', () => {
+            projectsHandler(index);
+        });
+    });
+
+    projectsCloseBtn.addEventListener('click', closeHandler);
+    projectsOpacityLayer.addEventListener('click', closeHandler);
 });
